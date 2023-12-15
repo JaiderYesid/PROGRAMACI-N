6768e7a0423befb25701b6c8eb96d5144498b7f0
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2023-12-15 11:25:41 -0500

    Create FOR.java

---
+++ FOR.java
@@ -0,0 +1,11 @@
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 20; i += 2) {
+		    System.out.println(i);
+		}
+
+	}
+
+}
