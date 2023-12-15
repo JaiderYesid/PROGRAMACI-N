4392364234012f1a036a72c82872dd6a45a2f6cf
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2023-12-15 11:27:58 -0500

    Create FOR2.java

---
+++ FOR2.java
@@ -0,0 +1,15 @@
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		for (int fila = 0; fila < matriz.length; fila++) {
+		    for (int columna = 0; columna < matriz[fila].length; columna++) {
+		        System.out.print(matriz[fila][columna] + " ");
+		    }
+		    System.out.println();
+		}
+
+	}
+
+}
